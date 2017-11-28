8a1280445087c2dd00aa8f26aa6b50b33c6946a5
Samer <samaladhami@gmail.com>
2017-11-27 23:22:32 -0600

    changing img urls

--- src/homeCtrl.js
+++ src/homeCtrl.js
@@ -1,13 +1,5 @@
 angular.module('ips')
 .controller('homeCtrl' , function($scope, mainService, $location){
-    // $scope.highlightLinks = function(e){
-    //     console.dir(e)
-    //     var headerLinks = document.querySelectorAll('.header-links a');
-    //     for(var i = 0; i < headerLinks.length; i++){
-    //         headerLinks[i].classList.remove('router-active')
-    //     }
-    //     angular.element(e.target).addClass('router-active')
-    // }
     $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
